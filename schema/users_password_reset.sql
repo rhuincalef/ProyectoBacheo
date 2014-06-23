316b299721619980ef72f61f7ca36bd27ebe62a8
pablo1n7 <pablo1n7@gmail.com>
2014-06-23 08:29:58 -0300

    script users.sql y users_password_reset.sql sql traducidos a postgres

--- schema/users_password_reset.sql
+++ schema/users_password_reset.sql
@@ -1,7 +1,8 @@
-DROP TABLE IF EXISTS `users_password_reset`;
-CREATE TABLE IF NOT EXISTS `users_password_reset` (
-  `password_reset_code` varchar(80) NOT NULL,
-  `user_id` bigint(20) NOT NULL,
-  `password_reset_date` datetime NOT NULL,
-  PRIMARY KEY  (`password_reset_code`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*DROP TABLE users_password_reset;*/
+CREATE TABLE users_password_reset (
+  password_reset_code varchar(80) NOT NULL,
+  user_id int NOT NULL,
+  password_reset_date date NOT NULL,
+  PRIMARY KEY  (password_reset_code),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+)

4caf349e43e1f94973e6e0079b2f1b5a92422551
pablo1n7 <pablo1n7@gmail.com>
2014-06-26 09:17:19 -0300

    Desarrollando el header y layout php

--- web/application/views/header.php
+++ web/application/views/header.php
@@ -1,19 +1,5 @@
-
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Bacheo trelew</title>
-  <link href="<?php echo $this->config->base_url(); ?>_/css/fontAwesome/font-awesome.css" rel="stylesheet">
-	<link href="<?php echo $this->config->base_url(); ?>_/css/bootstrap/bootstrap.min.css" rel="stylesheet">
-	<link href="<?php echo $this->config->base_url(); ?>_/css/header.css" rel="stylesheet">
-  <script src="<?php echo $this->config->base_url(); ?>_/js/libs/jquery-2.1.1.min.js"></script>
-	<script src="<?php echo $this->config->base_url(); ?>_/js/libs/bootstrap/bootstrap.min.js"></script>
-    <script src="<?php echo $this->config->base_url(); ?>_/js/plugins.js"></script>
-  <script src="<?php echo $this->config->base_url(); ?>_/js/header.js"></script>
-
-</head>
-<body>
-
+<link href="<?php echo $this->config->base_url(); ?>_/css/header.css" rel="stylesheet">
+<script src="<?php echo $this->config->base_url(); ?>_/js/header.js"></script>
 <div class="container">
 
       <!-- Static navbar -->

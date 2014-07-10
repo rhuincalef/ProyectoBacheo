4462e258892b0b71f38d96046e5ac3673c6674e3
Rodrigo <wynca.748@gmail.com>
2014-07-10 22:58:20 +0200

    Alta Baja bache en proceso

---
+++ web/application/views/BacheView.php
@@ -0,0 +1,7 @@
+<?php 
+	echo "<b>Vista renderizada hehe</b><br><br>";
+	echo "<b>Lo que se retorno de la BD convertido a JSON fue:";
+	echo $respuestaJSON;
+/* End of file BacheView.php */
+/* Location: ./application/views/BacheView.php */
+?>

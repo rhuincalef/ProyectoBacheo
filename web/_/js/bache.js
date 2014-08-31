3e5f070ebffe63f0bc9c7edab589e0f34f15d7c2
Ender <ender@ubuntu.(none)>
2014-08-31 17:43:21 -0300

    acomodando cosas, y comenzando a crear los modelos JS para la vista de Bache

--- web/_/js/bache.js
+++ web/_/js/bache.js
@@ -1,20 +1,13 @@
 $(document).ready(function(){
-	var mapOptions = {
-          center: new google.maps.LatLng(-43.253432, -65.310137),
-          zoom: 16,
-          mapTypeControl: false,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-    var map = new google.maps.Map(document.getElementById("canvasMapa"),mapOptions);
-
+	Bache.init();
     obtenerComentarios();
 });
 
 
 
 function obtenerComentarios() {
-	// body...
-	comentarios= '[{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"}]';
+//	comentarios= '[{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"},{"comentario":"una cagada este bache", "usuario":"Doe","fecha":"11/12/1980"}]';
+	var comentarios = Bache.comentarios();
 	cargarComentarios(JSON.parse(comentarios));
 }
 
--- web/application/views/templates/header/bache.php
+++ web/application/views/templates/header/bache.php
@@ -1,2 +1,3 @@
 <link href="<?php echo $this->config->base_url(); ?>_/css/bache.css" rel="stylesheet">
+<script src="<?php echo $this->config->base_url(); ?>_/js/bache.modelo.js"></script>
 <script src="<?php echo $this->config->base_url(); ?>_/js/bache.js"></script>
